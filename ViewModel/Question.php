764022e21ae57ebcf0cfcd9b2222709730ca4b9f
sebalang1 <88249783+sebalang1@users.noreply.github.com>
2022-03-27 20:56:47 +0200

    Basic rendering of questions

---
+++ ViewModel/Question.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace SebTech\FAQTwo\ViewModel;
+
+use Magento\Framework\Api\SearchCriteriaInterface;
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+use SebTech\FAQTwo\Api\Data\QuestionSearchResultsInterface;
+use SebTech\FAQTwo\Model\QuestionRepository;
+use SebTech\FAQTwo\Model\ResourceModel\Question\Collection as QuestionCollection;
+
+class Question implements ArgumentInterface
+{
+    private QuestionRepository $questionRepository;
+    private QuestionCollection $collectionQuestion;
+
+    /**
+     * @param QuestionRepository $questionRepository
+     * @param QuestionCollection $collectionQuestion
+     */
+    public function __construct(
+        QuestionRepository $questionRepository,
+        QuestionCollection $collectionQuestion
+
+    ) {
+        $this->questionRepository = $questionRepository;
+        $this->collectionQuestion = $collectionQuestion;
+    }
+
+    /**
+     * @return \Magento\Framework\DataObject[]
+     */
+    public function getQuestions()
+    {
+        return $this->collectionQuestion->getItems();
+    }
+}
